70bbe8a300a5e877c4fb3cddb719cffc9ce238ac
Aaliyah Montgomery <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-23 17:57:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,16 +12,15 @@ setup(
     description='Easily convert Excel invoices into professional PDF formatted invoices, with this Python package, streamlining your invoicing workflow.',
     author=os.getenv("AUTHOR"),
     author_email=os.getenv("EMAIL"),
-    url=os.getenv("URL"),  # Homepage of your library (e.g. GitHub or your website)
+    url=os.getenv("URL"),  
     keywords=['invoice', 'excel', 'pdf'],
     install_requires=['fpdf', 'openpyxl', "pandas"],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Who is the audience for your library?
+        'Intended Audience :: Developers',  
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',  # Python versions that your library supports
+        'Programming Language :: Python :: 3.8',  # Python versions that gen-invoices library supports
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
